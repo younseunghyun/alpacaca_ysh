25bf86e19bdc3898fc77b7aa73f982688f2c33b9
yunseunghyun <rogve22@gmail.com>
2017-12-10 20:47:35 +0900

    [모델링-004] LSTM 모델 수정 (output use not softmax)

--- model/rnn.py
+++ model/rnn.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from torch.autograd import Variable
 
+
 class RNN(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(RNN, self).__init__()
@@ -26,30 +27,30 @@ class RNN(nn.Module):
     def initHidden(self):
         return Variable(torch.zeros(1, self.hidden_size))
 
+
 class BasicLSTM(nn.Module):
     def __init__(self, batch_size, input_size, rnn_size, hidden_size, target_size):
         super(BasicLSTM, self).__init__()
-        
+
         self.batch_size = batch_size
         self.rnn_size = rnn_size
         self.i2h = nn.Linear(input_size, hidden_size)
-        
+
         self.lstm = nn.LSTM(hidden_size, rnn_size, batch_first=True)
-        
+
         self.hidden2out = nn.Linear(rnn_size, target_size)
         self.hidden = self.init_hidden()
-        
+
         self.softmax = nn.LogSoftmax()
 
     def forward(self, inputs):
         # inputs : (batch_size, seq_size, embedding_size)
-        
+
         embeds = self.i2h(inputs)
         lstm_out, self.hidden = self.lstm(embeds, self.hidden)
         # print(lstm_out.size())
         output_space = self.hidden2out(lstm_out)
-        output_scores = self.softmax(output_space)
-        return output_scores
+        return output_space
 
     def init_hidden(self):
         return (Variable(torch.zeros(1, self.batch_size, self.rnn_size)),
